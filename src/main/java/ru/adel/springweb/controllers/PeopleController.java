60853b37995b500de81b0ec4c37986212b2a2570
Saqqqner <rikardofik@gmail.com>
2023-01-19 19:29:06 +0300

    V0.1 post

--- src/main/java/ru/adel/springweb/controllers/PeopleController.java
+++ src/main/java/ru/adel/springweb/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.adel.springweb.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.adel.springweb.dao.PersonDAO;
+import ru.adel.springweb.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -30,6 +29,14 @@ public class PeopleController {
         // Получим одного человека из DAO и передадим на отображение в представление
         model.addAttribute("person",personDAO.show(id));
         return "people/show";
-
+    }
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person")Person person){
+        return "people/new";
+    }
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
--- src/main/java/ru/adel/springweb/dao/PersonDAO.java
+++ src/main/java/ru/adel/springweb/dao/PersonDAO.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-    private List<Person> people;
+    private final List<Person> people;
     {
         people= new ArrayList<>();
         people.add(new Person(++PEOPLE_COUNT,"Tom"));
@@ -21,6 +21,11 @@ public class PersonDAO {
     }
     public Person show (int id){
         return people.stream().filter(person->person.getId()==id).findAny().orElse(null);
+    }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
     }
 
 }
